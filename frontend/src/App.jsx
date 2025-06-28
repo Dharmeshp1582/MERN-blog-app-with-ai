1f1171f16f8c8b257ec05f424585f347bc9e9fad
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-06-28 20:02:39 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+
+const App = () => {
+  return (
+    <di>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+         <Route path='/blog/:id' element={<Blog/>}></Route>
+      </Routes>
+    </di> 
+  )  
+}
+
+export default App
